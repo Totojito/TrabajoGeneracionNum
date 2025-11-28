a7c4b1a4af0c8e3b1bf764e21e5202711925f539
axela <axela@AXEL>
2025-11-27 23:46:20 -0600

    Agregar archivos de proyecto.

---
+++ TrabajoLun/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TrabajoLun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.btnSelectionSort = new System.Windows.Forms.Button();
+            this.btnMergeSort = new System.Windows.Forms.Button();
+            this.btnJumpSearch = new System.Windows.Forms.Button();
+            this.btnInterpolation = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(12, 25);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Generar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.ItemHeight = 16;
+            this.lstNumeros.Location = new System.Drawing.Point(13, 54);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(120, 292);
+            this.lstNumeros.TabIndex = 1;
+            // 
+            // btnSelectionSort
+            // 
+            this.btnSelectionSort.Location = new System.Drawing.Point(151, 123);
+            this.btnSelectionSort.Name = "btnSelectionSort";
+            this.btnSelectionSort.Size = new System.Drawing.Size(156, 40);
+            this.btnSelectionSort.TabIndex = 2;
+            this.btnSelectionSort.Text = "Selection Sort (seleccion)";
+            this.btnSelectionSort.UseVisualStyleBackColor = true;
+            this.btnSelectionSort.Click += new System.EventHandler(this.btnSelectionSort_Click);
+            // 
+            // btnMergeSort
+            // 
+            this.btnMergeSort.Location = new System.Drawing.Point(151, 185);
+            this.btnMergeSort.Name = "btnMergeSort";
+            this.btnMergeSort.Size = new System.Drawing.Size(156, 37);
+            this.btnMergeSort.TabIndex = 3;
+            this.btnMergeSort.Text = "Merge Sort (mescla)";
+            this.btnMergeSort.UseVisualStyleBackColor = true;
+            this.btnMergeSort.Click += new System.EventHandler(this.btnMergeSort_Click);
+            // 
+            // btnJumpSearch
+            // 
+            this.btnJumpSearch.Location = new System.Drawing.Point(151, 246);
+            this.btnJumpSearch.Name = "btnJumpSearch";
+            this.btnJumpSearch.Size = new System.Drawing.Size(156, 35);
+            this.btnJumpSearch.TabIndex = 4;
+            this.btnJumpSearch.Text = "Búsqueda por Saltos";
+            this.btnJumpSearch.UseVisualStyleBackColor = true;
+            this.btnJumpSearch.Click += new System.EventHandler(this.btnJumpSearch_Click);
+            // 
+            // btnInterpolation
+            // 
+            this.btnInterpolation.Location = new System.Drawing.Point(151, 308);
+            this.btnInterpolation.Name = "btnInterpolation";
+            this.btnInterpolation.Size = new System.Drawing.Size(156, 38);
+            this.btnInterpolation.TabIndex = 5;
+            this.btnInterpolation.Text = "Búsqueda Interpolada";
+            this.btnInterpolation.UseVisualStyleBackColor = true;
+            this.btnInterpolation.Click += new System.EventHandler(this.btnInterpolation_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(151, 26);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(180, 22);
+            this.txtBuscar.TabIndex = 6;
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(9, 360);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(97, 16);
+            this.lblTiempo.TabIndex = 7;
+            this.lblTiempo.Text = "Tiempo: 0 ticks";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 400);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnInterpolation);
+            this.Controls.Add(this.btnJumpSearch);
+            this.Controls.Add(this.btnMergeSort);
+            this.Controls.Add(this.btnSelectionSort);
+            this.Controls.Add(this.lstNumeros);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.Button btnSelectionSort;
+        private System.Windows.Forms.Button btnMergeSort;
+        private System.Windows.Forms.Button btnJumpSearch;
+        private System.Windows.Forms.Button btnInterpolation;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label lblTiempo;
+    }
+}
+
---
+++ TrabajoLun/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics; 
+using System.Linq; 
+using System.Windows.Forms;
+
+namespace TrabajoLun
+{
+    public partial class Form1 : Form
+    {
+
+        private List<int> Numeros;
+
+        public Form1()
+        {
+            InitializeComponent();
+ 
+            Numeros = new List<int>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Aqui se generan 500,000 números aleatorios
+            // se puede cambiar a 1,000,000 y 5,000,000 para probar
+
+            int cantidad = 500000;
+
+            lstNumeros.DataSource = null;
+            lstNumeros.Items.Clear();
+            Numeros = null;
+            GC.Collect();
+
+            Numeros = new List<int>(cantidad);
+            Random rnd = new Random();
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Numeros.Add(rnd.Next(1, cantidad * 2));
+            }
+
+            ActualizarListaVisual(); 
+
+            this.Text = $"Datos en memoria: {cantidad:N0}";
+            lblTiempo.Text = "Tiempo: Lista generada.";
+        }
+
+
+        private void btnSelectionSort_Click(object sender, EventArgs e)
+        {
+  
+            // Con 500,000 datos tardaría horas. Con 5M, días por eso tiene ese limite para que no se pegue.
+
+            if (Numeros.Count > 20000)
+            {
+                MessageBox.Show("\nSelection Sort es demasiado lento para esta cantidad de datos.\n" +
+                                "\nLa aplicación se congelaría.\n" +
+                                "\nUsa Merge Sort o reduce la cantidad de datos a menos de 20,000.\n",
+                                "Protección de Algoritmo");
+                return;
+            }
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            // Algoritmo Selection Sort
+            int n = Numeros.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (Numeros[j] < Numeros[min_idx])
+                        min_idx = j;
+                }
+                int temp = Numeros[min_idx];
+                Numeros[min_idx] = Numeros[i];
+                Numeros[i] = temp;
+            }
+
+            sw.Stop();
+            ActualizarListaVisual();
+            lblTiempo.Text = $"Selection Sort: {sw.ElapsedMilliseconds} ms";
+            MessageBox.Show("Ordenado con Selection Sort");
+        }
+
+        private void btnMergeSort_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+
+                Numeros = MergeSort(Numeros);
+
+                sw.Stop();
+                ActualizarListaVisual();
+                lblTiempo.Text = $"Merge Sort: {sw.ElapsedMilliseconds} ms";
+                MessageBox.Show("Ordenado con Merge Sort");
+            }
+            catch (StackOverflowException)
+            {
+                MessageBox.Show("Error: La recursividad fue demasiado profunda para la memoria de la pila (Stack).");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error inesperado: " + ex.Message);
+            }
+        }
+
+
+        private List<int> MergeSort(List<int> lista)
+        {
+            if (lista.Count <= 1) return lista;
+            int mid = lista.Count / 2;
+            List<int> left = lista.GetRange(0, mid);
+            List<int> right = lista.GetRange(mid, lista.Count - mid);
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>(left.Count + right.Count);
+            int i = 0, j = 0;
+            while (i < left.Count && j < right.Count)
+            {
+                if (left[i] <= right[j]) result.Add(left[i++]);
+                else result.Add(right[j++]);
+            }
+            while (i < left.Count) result.Add(left[i++]);
+            while (j < right.Count) result.Add(right[j++]);
+            return result;
+        }
+
+
+        private void btnJumpSearch_Click(object sender, EventArgs e)
+        {
+
+            Numeros.Sort();
+            ActualizarListaVisual(); 
+
+            int x;
+            if (int.TryParse(txtBuscar.Text, out x))
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                int index = JumpSearch(Numeros, x);
+                sw.Stop();
+
+                lblTiempo.Text = $"Jump Search: {sw.ElapsedTicks} ticks";
+                MostrarResultado(index, x);
+            }
+        }
+
+        private int JumpSearch(List<int> arr, int x)
+        {
+            int n = arr.Count;
+            int step = (int)Math.Sqrt(n);
+            int prev = 0;
+            while (arr[Math.Min(step, n) - 1] < x)
+            {
+                prev = step;
+                step += (int)Math.Sqrt(n);
+                if (prev >= n) return -1;
+            }
+            while (arr[prev] < x)
+            {
+                prev++;
+                if (prev == Math.Min(step, n)) return -1;
+            }
+            if (arr[prev] == x) return prev;
+            return -1;
+        }
+
+        private void btnInterpolation_Click(object sender, EventArgs e)
+        {
+            Numeros.Sort();
+            ActualizarListaVisual();
+
+            int x;
+            if (int.TryParse(txtBuscar.Text, out x))
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                int index = InterpolationSearch(Numeros, x);
+                sw.Stop();
+
+                lblTiempo.Text = $"Interpolation: {sw.ElapsedTicks} ticks";
+                MostrarResultado(index, x);
+            }
+        }
+
+        private int InterpolationSearch(List<int> sortedList, int x)
+        {
+            int lo = 0;
+            int hi = sortedList.Count - 1;
+            while (lo <= hi && x >= sortedList[lo] && x <= sortedList[hi])
+            {
+                if (lo == hi) return sortedList[lo] == x ? lo : -1;
+
+                long pos = lo + (((long)(hi - lo) * (x - sortedList[lo])) / (sortedList[hi] - sortedList[lo]));
+
+                if (sortedList[(int)pos] == x) return (int)pos;
+                if (sortedList[(int)pos] < x) lo = (int)pos + 1;
+                else hi = (int)pos - 1;
+            }
+            return -1;
+        }
+
+        private void ActualizarListaVisual()
+        {
+       
+            lstNumeros.DataSource = null;
+
+            if (Numeros.Count > 100)
+            {
+                // Si hay muchos datos, solo se muestran los primeros 100
+                // Usando .Take(100)
+                lstNumeros.DataSource = Numeros.Take(100).ToList();
+            }
+            else
+            {
+                // Si son poquitos, mostramos todo
+                lstNumeros.DataSource = Numeros;
+            }
+        }
+
+        private void MostrarResultado(int index, int valorBuscado)
+        {
+            if (index != -1)
+            {
+                // Si el índice encontrado está dentro de los primeros 100 (la vista previa)
+                if (index < 100)
+                {
+                    lstNumeros.SelectedIndex = index;
+                }
+
+                MessageBox.Show($"\nEl número {valorBuscado} está en la posición (índice): {index}\n" +
+                                $"\nla lista visual solo muestra los primeros 100\n");
+            }
+            else
+            {
+                MessageBox.Show("Numero no encontrado.");
+            }
+        }
+    }
+}
